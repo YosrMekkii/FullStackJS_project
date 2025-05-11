38038ee2c2797007e98d2f39691190a8768c9249
YosrMekkii <yosr.mekki@esprit.tn>
2025-05-11 09:54:40 +0100

    Update MarketPlace.tsx

--- piFrontEnd/src/pages/MarketPlace.tsx
+++ piFrontEnd/src/pages/MarketPlace.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserPlus, PlusCircle, Flag, Search, Heart, Edit, Trash2 } from "lucide-react";
 import Sidebar from "../components/Sidebar";
